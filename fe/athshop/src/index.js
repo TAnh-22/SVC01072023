2f719f98fa704acfc9f30ada3bb2b4dcf512bc1d
TAnh-22 <tuananhkubi2002@gmail.com>
2025-03-27 19:30:51 +0700

    done

--- fe/athshop/src/index.js
+++ fe/athshop/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
+import{BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>ATH SHOP</h1>
+    <BrowserRouter>
+
+    </BrowserRouter>
 );
